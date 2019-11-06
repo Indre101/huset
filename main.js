b4699cb628383ab5f72a82b1b95faa6e040245e0
indre101 <zygaityte.indre@gmail.com>
2019-11-06 20:20:19 +0100

    added readmore/less btn

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const mbMenuIcon = document.querySelector(".mb-menu");
 const navigationMenu = document.querySelector("nav");
+const menuItems = document.querySelectorAll(".menu-item")
+const readmore = document.querySelector(".readmore");
+const textAboutHuset = document.querySelector(".textAboutHuset");
 
-console.log(navigationMenu)
+
+
+
 
 const displayNoneClass = "d-none";
 const showMenuClass = "navigation";
@@ -10,15 +15,42 @@ const menuIconClosed = "menu-icon-closed";
 const menuIconOpen = "menu-icon-open";
 
 
+const textAboutHusetCollapsedClass = "textAboutHusetCollapsed";
+const textAboutHusetExpandedClass = "textAboutHusetExpanded";
+
+readmore.onclick = function () {
+
+
+  toggleBetweenTwoClasses(textAboutHuset, textAboutHusetCollapsedClass, textAboutHusetExpandedClass)
+
+
+  if (readmore.textContent === "Read Less") {
+    readmore.textContent = "Read More"
+
+  } else if (readmore.textContent === "Read More") {
+    readmore.textContent = "Read Less"
+
+  }
+
+}
+
+
+
+
+menuItems.forEach(item => {
+  item.onclick = function () {
+    toggleBetweenTwoClasses(navigationMenu, showMenuClass, displayNoneClass)
+    toggleBetweenTwoClasses(mbMenuIcon, menuIconOpen, menuIconClosed)
+
+
+  }
+})
 
 mbMenuIcon.onclick = function () {
   toggleBetweenTwoClasses(mbMenuIcon, menuIconClosed, menuIconOpen)
   toggleBetweenTwoClasses(navigationMenu, displayNoneClass, showMenuClass)
 
 }
-
-
-
 
 
 function toggleBetweenTwoClasses(element, classNameToRemove, classNameToAdd) {
