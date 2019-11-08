e5ec911f50bbbbbc47fdc3d531b01600094c6d3d
indre101 <zygaityte.indre@gmail.com>
2019-11-08 21:28:14 +0100

    refactored the code a bit

--- script.js
+++ script.js
@@ -2,137 +2,114 @@ const eventTemplate = querySelectorElement(".eventTemplate").content;
 const events = querySelectorElement(".events");
 const inputTemplate = querySelectorElement(".inputTemplate").content
 
-fetch("http://timidesign.org/kea/wordpress-excersize/wordpress/wordpress/wp-json/wp/v2/schedule?per_page=100").then(res => {
+fetch("http://timidesign.org/kea/wordpress-excersize/wordpress/wordpress/wp-json/wp/v2/schedule?_embed&per_page=100").then(res => {
   return res.json()
-}).then(showData);
+}).then(data => {
+  data.forEach(showData)
+});
 
 
-
-const noDoublicates = (arr) =>
-  arr.filter(function (item, index) {
-    return arr.indexOf(item) >= index;
-  });
-
-const categories = []
+fetch("http://timidesign.org/kea/wordpress-excersize/wordpress/wordpress/wp-json/wp/v2/categories").then(res => {
+  return res.json()
+}).then(data => {
+  data.forEach(cerateCategories)
+});
 
 
+// const noDoublicates = (arr) =>
+//   arr.filter(function (item, index) {
+//     return arr.indexOf(item) >= index;
+//   });
+
+const options = querySelectorElement(".options");
 
 
-function showData(data) {
+function showData(item) {
 
-
-  data.forEach(item => {
-    item.event_name.forEach(event => {
-
-      const cln = eventTemplate.cloneNode(true);
-      const eventCard = cln.querySelector(".event");
-      cln.querySelector(".eventName").textContent = event.event_name;
-      cln.querySelector(".date").textContent = item.event_date;
-      // console.log(event)
-      event.category.forEach(category => {
-        let list = document.createElement("h3");
-        list.textContent = category.name;
-        cln.querySelector(".categories").appendChild(list);
-      })
-
-
-      // console.log(eventCard)
-
-
-      cln.querySelector(".eventHours").textContent = `Event starts ${event.event_time}/ Doors open ${event.door_opening_time}`
-      cln.querySelector(".price").textContent = event.price;
-      cln.querySelector(".description").textContent = event.post_content;
-
-
-      event.category.forEach(cat => {
-
-        eventCard.classList.add(`${cat.name.toLowerCase().split(' ').join('')}`)
-        categories.push(cat.name);
-
-      })
-
-      const eventExtrainformationContainer = cln.querySelector(".eventExtrainformationContainer");
-      const eventExtrainformation = cln.querySelector(".eventExtrainformation");
-
-      const eventHeightfitClass = "eventHeightfit";
-      const eventHeightExpand = "eventHeightExpand";
-
-
-      eventCard.onclick = function () {
-        toggleBetweenTwoClasses(eventExtrainformation, eventHeightfitClass, eventHeightExpand);
-
-      }
-
-
-      events.appendChild(cln)
-    })
-    // console.log(item.event_name)
-
-  })
-
-
-
-
-
-  // Creates categories and appends them as input fields
-  const filteredCategories = noDoublicates(categories)
-  filteredCategories.forEach(cerateCategories)
-
+  item.event_name.forEach(showValues)
   // All events selected
   const eventCards = querySelectAll(".event");
-
-  eventCards.forEach(item => {
-    // console.log(item)
-  })
-  // console.log(eventCards)
-
-
+  // Checks if any input is selected;
   const inputs = querySelectAll(".check")
-  inputs.forEach(inputItem => {
-    // console.log(inputItem);
-
-    // inputItem.addEventListener("click", getTheInputValue)
-  })
-
-}
-
-// let inputsChecked = [];
-
-// const getTheInputValue = () => {
-
-//   console.log(this);
-//   inputsChecked.push(this );
-// }
 
 
-const filterByEventType = (input, nodeList) => {
+  submitBtn.onclick = function () {
+    // toggleBetweenTwoClasses(filterForm, displayFlex, displayNoneClass);
+    const checkedInputsNames = () => inputs.forEach(getCheckedInputName)
+    const getCheckedInputName = (inputItem) => inputItem.checked ? checkedInputs.push(inputItem.name) : false;
 
 
-  // element.classList.contains(class);
+    checkedInputs.forEach(i => {
+      console.log(i)
+    })
 
-
-}
-
-const cerateCategories = (cat) => {
-
-  console.log(cat)
-
-
-  let clnInput = inputTemplate.cloneNode(true);
-  let checkboxName = clnInput.querySelector(".checkboxName")
-  let check = clnInput.querySelector(".check");
-  checkboxName.textContent = cat;
-  check.name = cat.toLowerCase().split(' ').join('');
+    // console.log(checkedInputs)
+    checkedInputsNames()
+    eventCards.forEach(item => {
 
 
 
-  checkboxName.onclick = function () {
-    checkIfChecked(check);
+      // console.log(item);
+      // console.log(item.classList.contains(checkedInputsNames()));
 
+      // console.log(item)
+    })
+    // console.log(eventCards)
   }
 
-  querySelectorElement(".options").appendChild(clnInput);
+}
 
+
+let checkedInputs = []
+
+
+
+const showValues = (event, item) => {
+  const cln = eventTemplate.cloneNode(true);
+  const eventCard = cln.querySelector(".event");
+  cln.querySelector(".eventName").textContent = event.event_name;
+  cln.querySelector(".date").textContent = item.event_date;
+  // cln.querySelector(".eventImg").src = event._embedded["wp:featuredmedia"][0].href;
+  cln.querySelector(".eventHours").textContent = `Event starts ${event.event_time}/ Doors open ${event.door_opening_time}`
+  cln.querySelector(".price").textContent = event.price;
+  cln.querySelector(".description").textContent = event.post_content;
+  event.category.forEach(category => {
+    let list = document.createElement("h3");
+    list.textContent = category.name;
+    cln.querySelector(".categories").appendChild(list);
+    eventCard.classList.add(`${category.name.toLowerCase().split(' ').join('')}`)
+
+  })
+  const eventExtrainformationContainer = cln.querySelector(".eventExtrainformationContainer");
+  const eventExtrainformation = cln.querySelector(".eventExtrainformation");
+  const eventHeightfitClass = "eventHeightfit";
+  const eventHeightExpand = "eventHeightExpand";
+  eventCard.onclick = function () {
+    toggleBetweenTwoClasses(eventExtrainformation, eventHeightfitClass, eventHeightExpand);
+
+  }
+  events.appendChild(cln)
+}
+
+
+
+
+
+const cerateCategories = (cat) => {
+  if (cat.parent == 0) {
+    return false
+  } else {
+    console.log(cat)
+    let clnInput = inputTemplate.cloneNode(true);
+    let checkboxName = clnInput.querySelector(".checkboxName")
+    let check = clnInput.querySelector(".check");
+    checkboxName.textContent = cat.name;
+    check.name = cat.name.toLowerCase().split(' ').join('');
+    checkboxName.onclick = function () {
+      checkIfChecked(check);
+    }
+    querySelectorElement(".options").appendChild(clnInput);
+  }
 }
 
 
@@ -141,15 +118,11 @@ const cerateCategories = (cat) => {
 
 
 const checkIfChecked = (inputName) => {
-
   if (inputName.checked === false) {
     inputName.checked = true
-
   } else if (inputName.checked === true) {
     inputName.checked = false
-
   }
-
 }
 
 
@@ -157,15 +130,8 @@ const checkIfChecked = (inputName) => {
 const filterIcon = querySelectorElement(".filterIcon");
 const filterForm = querySelectorElement(".filter");
 const submitBtn = querySelectorElement(".submitBtn");
-
 const displayFlex = "d-flex"
 
 filterIcon.onclick = function () {
   toggleBetweenTwoClasses(filterForm, displayNoneClass, displayFlex);
-
-}
-
-submitBtn.onclick = function () {
-  toggleBetweenTwoClasses(filterForm, displayFlex, displayNoneClass);
-
 }
