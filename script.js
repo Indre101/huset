c5f7cc4587eb6c7c169c9fc451fba2afb8be6b78
indre101 <zygaityte.indre@gmail.com>
2019-11-10 17:01:46 +0100

    date filter works

--- script.js
+++ script.js
@@ -96,6 +96,7 @@ const currentDate = getCurrentDate();
 
 function compareDates(schedule) {
   const dateA = new Date(schedule.event_date).getTime()
+  // const dateZ = new Date(currentDate).toLocaleDateString("eu-EU");
   if (dateA >= currentDate) {
     return true
   } else if (dateA < currentDate) {
@@ -106,30 +107,99 @@ function compareDates(schedule) {
 const options = querySelectorElement(".options");
 let checkedInputs = [];
 let uncheckedInputs = []
+let active = "active";
+let hide = "hide"
 
 function filterData() {
   const events = querySelectAll(".event");
   submitBtn.onclick = function () {
     toggleBetweenTwoClasses(filterForm, displayFlex, displayNoneClass);
     getCheckedInputs();
-    for (let m = 0; m < events.length; m++) {
-      for (let n = 0; n < uncheckedInputs.length; n++) {
-        if (events[m].classList.contains(uncheckedInputs[n])) {
-          events[m].classList.remove("active")
-          events[m].classList.add("hide")
-        }
-      }
+
+    if (checkedInputs.length === 0) {
+      events.forEach(e => {
+        e.classList.remove("hide")
+        e.classList.add("active")
+      })
+
+    } else if (checkedInputs.length > 0) {
+
+      getEventsThatHaveTheClass(events, uncheckedInputs, active, hide)
+      getEventsThatHaveTheClass(events, checkedInputs, hide, active)
+      console.log(checkedInputs)
+      // for (let m = 0; m < events.length; m++) {
+      //   for (let n = 0; n < uncheckedInputs.length; n++) {
+      //     if (events[m].classList.contains(uncheckedInputs[n])) {
+      //       events[m].classList.remove("active")
+      //       events[m].classList.add("hide")
+
+      //     }
+      //   }
+      // }
+
     }
-    for (let i = 0; i < events.length; i++) {
-      for (let j = 0; j < checkedInputs.length; j++) {
-        if (events[i].classList.contains(checkedInputs[j])) {
-          events[i].classList.remove("hide")
-          events[i].classList.add("active")
-        }
+
+  }
+}
+
+function getEventsThatHaveTheClass(eventsArr, inputsArr, class1, class2) {
+  console.log("kjkjkl")
+  for (let i = 0; i < eventsArr.length; i++) {
+    for (let j = 0; j < inputsArr.length; j++) {
+      if (eventsArr[i].classList.contains(inputsArr[j])) {
+        eventsArr[i].classList.remove(class1)
+        eventsArr[i].classList.add(class2)
+        filterByDate(eventsArr[i])
       }
     }
   }
 }
+
+const clearBtn = querySelectorElement(".clear");
+clearBtn.addEventListener("click", clearFilters)
+
+
+function clearFilters() {
+  dateInput1.valueAsDate = null
+  dateInput2.valueAsDate = null
+  checkedInputs = [];
+  uncheckedInputs = []
+  const inputs = querySelectAll(".check")
+  inputs.forEach(i => {
+    i.checked = false
+  })
+
+}
+
+
+const dateInput1 = querySelectorElement(".date1");
+const dateInput2 = querySelectorElement(".date2");
+const dates = querySelectAll(".date")
+const erMessage = querySelectorElement(".error")
+
+
+
+function filterByDate(oneEvent) {
+  erMessage.classList.remove("d-block")
+  erMessage.classList.add("d-none")
+  const eventElement = new Date(oneEvent.querySelector(".date").textContent).getTime();
+  const date1 = new Date(dateInput1.value).getTime();
+  const date2 = new Date(dateInput2.value).getTime();
+  if (!date1 || !date2) {
+    return false
+  } else if (date1 > date2) {
+    erMessage.classList.add("d-block")
+    erMessage.classList.remove("d-none")
+    return false
+  } else if ((eventElement < date1) || (eventElement > date2)) {
+    oneEvent.classList.remove("active");
+    oneEvent.classList.add("hide");
+    return false
+  } else if ((eventElement >= date1) && (eventElement <= date2)) {
+    return true
+  }
+}
+
 
 function getCheckedInputs() {
   checkedInputs = [];
