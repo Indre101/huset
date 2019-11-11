bc438273bda7b8bb0fccf006788664c1df86454e
indre101 <zygaityte.indre@gmail.com>
2019-11-11 21:19:29 +0100

    added calendar

--- login.js
+++ login.js
@@ -74,7 +74,7 @@ closeLogIn.onclick = function () {
 
 }
 
-var getDaysInMonth = function (month, year) {
+let getDaysInMonth = function (month, year) {
   // Here January is 1 based
   //Day 0 is the last day in the previous month
   return new Date(year, month, 0).getDate();
@@ -83,12 +83,121 @@ var getDaysInMonth = function (month, year) {
 };
 
 
-function getDaysCount() {
-  for (let monthNumber = 1; monthNumber <= 12; monthNumber++) {
+let weekDays = getWeekDays('en-En')
 
-    const a = getDaysInMonth(monthNumber, 2019)
-    return a
+function getWeekDays(locale) {
+  let baseDate = new Date(Date.UTC(2017, 0, 2)); // just a Monday
+  let weekDays = [];
+  for (i = 0; i < 7; i++) {
+    weekDays.push(baseDate.toLocaleDateString(locale, {
+      weekday: 'long'
+    }));
+    baseDate.setDate(baseDate.getDate() + 1);
   }
+  return weekDays;
 }
-console.log(getDaysCount())
+
+
+console.log(weekDays)
+
+
+
+function getDayName(dateStr, locale) {
+  var date = new Date(dateStr);
+  return date.toLocaleDateString(locale, {
+    weekday: 'long'
+  });
+}
+
+
+
+
+
+
+const monthTemplate = querySelectorElement(".monthTemplate").content;
+const calendar = querySelectorElement(".calendar");
+
+// const Dates() {
+//   this.dayNumber = dayNumber,
+//     thhis.
+// }
+
+
+
+function getDaysCount() {
+  const daysArr = [];
+  for (let monthNumber = 1; monthNumber <= 12; monthNumber++) {
+    const daysCountInAMonth = getDaysInMonth(monthNumber, 2019)
+    daysArr.push(daysCountInAMonth)
+  }
+  return daysArr
+}
+
+
+let monthNumber = 0;
+const daysArr = getDaysCount()
+console.log(daysArr)
+daysArr.forEach(d => {
+
+  monthNumber++;
+  const clnMonth = monthTemplate.cloneNode(true);
+  const days = clnMonth.querySelector(".days");
+
+  for (let dayNumber = 1; dayNumber < d; dayNumber++) {
+
+    const day = document.createElement("div");
+    day.classList.add("day");
+    day.textContent = dayNumber;
+    let dateStr = `${dayNumber}/${monthNumber}/2019`;
+    let dayOftheWeek = getDayName(dateStr, "en-Eu");
+    console.log(dateStr)
+
+    const weekDayName = document.createElement("p");
+    weekDayName.textContent = dayOftheWeek
+
+    day.appendChild(weekDayName)
+    days.appendChild(day);
+    //       console.log(dateStr)
+
+  }
+
+  calendar.appendChild(clnMonth)
+
+
+})
+
+
+
+
+
+
+
+// function appendDays() {
+//   const clnMonth = monthTemplate.cloneNode(true);
+
+
+//   let monthNumber = 0;
+
+//   do {
+//     monthNumber++
+//     for (let b = 1; b <= getDaysCount(); b++) {
+//       const days = clnMonth.querySelector(".days");
+//       const day = document.createElement("div");
+//       day.classList.add("day");
+//       day.textContent = b;
+//       days.appendChild(day);
+//       let dateStr = `${b}/${monthNumber}/2019`;
+//       let dayOftheWeek = getDayName(dateStr, "nl-NL");
+//       console.log(dateStr)
+
+//     }
+//   } while (monthNumber <= 12);
+
+
+//   calendar.appendChild(clnMonth)
+
+// }
+
+
+// appendDays()
 // console.log(getDaysInMonth(2, 2020))
