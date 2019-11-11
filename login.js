45a2d807f723d63ebc568f3f6e1896b424f0e35a
indre101 <zygaityte.indre@gmail.com>
2019-11-11 10:54:31 +0100

    addwd sth

--- login.js
+++ login.js
@@ -1,18 +1,30 @@
-const userName = querySelectorElement(".logInInput")
+const userNameInput = querySelectorElement(".logInInput")
+const userPswInput = querySelectorElement(".userPsw")
+const logInBtn = querySelectorElement(".logInBtn")
 
-const trySth = () => {
-  console.log(userName)
-
-}
 
 fetch("https://timidesign.org/kea/wordpress-excersize/wordpress/wordpress/wp-json/wp/v2/schedule?_embed&per_page=100").then(res => {
   return res.json()
 }).then(data => {
-  console.log(data)
+  // console.log(data)
   data.forEach(showData)
 })
 
 
-function showDate(a) {
+function showData(a) {
   console.log(a)
+
+  logInBtn.onclick = function () {
+    event.preventDefault()
+    console.log(userNameInput.value)
+    console.log(userPswInput.value)
+    // console.log(a.pass)
+    // console.log(a.title.rendered)
+
+
+
+
+
+
+  }
 }
